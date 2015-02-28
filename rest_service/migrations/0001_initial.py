041a0818c2913abb17b285235293f9951d566725
k-pavlov <kaloian.pavlov@abv.bg>
2015-03-01 01:06:53 +0200

    Like and category models

---
+++ rest_service/migrations/0001_initial.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.db.models.deletion
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100, verbose_name=b'Name')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Like',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('time', models.DateTimeField(default=b'', auto_now_add=True, null=True, verbose_name=b'Time')),
+                ('user', models.ForeignKey(on_delete=django.db.models.deletion.SET_NULL, default=b'', blank=True, to=settings.AUTH_USER_MODEL, null=True, verbose_name=b'User')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Picture',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Story',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- rest_service/models.py
+++ rest_service/models.py
@@ -1,7 +1,32 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Story(models.Model):
+    pass
+
+    class Meta:
+        db_table = 'story'
+
+class Picture(models.Model):
+    
+    class Meta:
+        db_table = 'picture'
+
+class Category(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=100)
+
+    class Meta:
+        db_table = 'category'
+
+class Comment(models.Model):
+
+    class Meta:
+        db_table = 'comment'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, verbose_name='User', blank=True,
+                             null=True, default='', on_delete=models.SET_NULL)
+    time = models.DateTimeField(auto_now_add=True, blank=True, null=True, default='', verbose_name='Time')
+
+    class Meta:
+        db_table = 'like'
